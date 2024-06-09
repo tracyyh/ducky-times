e1dbe5dde779f33c4308b555ea281ffa1d3e32ca
tracyyh <huang.tr@northeastern.edu>
2024-06-09 17:33:37 -0400

    flow in the field frontend

--- ducky-times-app/src/App.js
+++ ducky-times-app/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './components/Header.js'
+import Nav from './components/nav'
 import Merch from './pages/merch.js';
 import Signup from './pages/signup.js';
 import Contact from './pages/contact.js';
@@ -10,7 +10,7 @@ import SignupThanks from './pages/signupThanks.js';
 function App () {
   return (
     <BrowserRouter>
-      <Header />
+      <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/merch' element={<Merch />} />
