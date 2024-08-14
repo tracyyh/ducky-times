ab2ea51ae216570f213e369bb7b521d7946c645b
tracyyh <huang.tr@northeastern.edu>
2024-08-14 01:54:35 -0400

    fix signup and signup thank you desktop

--- frontend/src/pages/signup/signup.tsx
+++ frontend/src/pages/signup/signup.tsx
@@ -7,8 +7,8 @@ const Merch = () => {
     <div className="content">
       <img className="thumb" src={thumb} alt="Ducky giving a thumbs up"></img>
       <div className="texts">
-        <h3>Ducky's Fan Club Registration</h3>
-        <p>
+        <h3 className="signupTitle">Ducky's Fan Club Registration</h3>
+        <p className="signupDesc">
           Ducky needs you to fill out this form if you're willing to let Ducky
           use the bathroom in your inbox every weekend.
         </p>
--- frontend/src/pages/signup/signupThanks.tsx
+++ frontend/src/pages/signup/signupThanks.tsx
@@ -4,10 +4,10 @@ import Salute from "../../components/images/Salute.png";
 
 const Merch = () => {
   return (
-    <div className="page">
+    <div className="pageThanks">
       <img className="Salute" src={Salute} alt="Ducky saluting"></img>
-      <nav className="texts">
-        <p>
+      <nav className="success">
+        <p className="successDesc">
           Your registration for the Ducky Fan Club was a success. See you every
           Monday at 9 am.
         </p>
